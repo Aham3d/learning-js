37a993d621684bccb94d3315a4a6e35e1bb03e4f
Moin Ahamed <ahamedjust@gmail.com>
2023-01-26 16:18:40 +0530

    basic coding challenges based on previous topics done

--- 6.code challenge/js/main.js
+++ 6.code challenge/js/main.js
@@ -1,29 +1,8 @@
-// math Methods and properties 
+// code challenge
 
-console.log(Math.PI)
-console.log(Math.trunc(Math.PI))
-console.log(Math.ceil(3.4)); //rounded up
-console.log(Math.floor(3.4)) //rounded down
+const nameRandom = "Moin Ahamed";
 
-
-console.log(Math.pow(2,10)) // 2 to the power of 10
-
-//returns the smallest number 
-console.log(Math.min(3,4,5))
-// vice verce
-console.log(Math.max(3,24,4))
-
-
-
-// random method
-console.log(Math.random()); // returns the random number
-
-
-//returns the number from 0-10
-console.log(Math.floor(Math.random() * 10))
-// to return from 1 - 10
-console.log(Math.floor(Math.random() * 10) +1); 
-
+console.log(nameRandom.charAt(Math.floor(Math.random() * nameRandom.length)));
 
 
 
