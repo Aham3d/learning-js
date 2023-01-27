b63555a2df50fc316b6fcd2dd7ffeb3e2c235a77
Moin Ahamed <ahamedjust@gmail.com>
2023-01-27 10:52:05 +0530

    game: improved version of rock, paper and scissor. using if statements, ternary operatros and user inputs

---
+++ 10.first game/js/main.js
@@ -0,0 +1,43 @@
+// first game
+
+let playGame = confirm("shall we play rock, paper and sciccor?");
+if(playGame) {
+
+    let playerChoice = prompt("enter your move");
+    if(playerChoice) {
+        let player = playerChoice.trim().toLowerCase();
+        if (player === "rock" || 
+            player === "paper" || 
+            player === "scissor") {
+
+            let computerchoice = Math.floor(Math.random() * 3 + 1); 
+            let computer = computerchoice === 1 ? "rock" 
+            : computerchoice === 2 ? "paper" 
+            : "scissor"; 
+
+            let result = player === computer ? "game tied!"
+            : player === "rock" && computer === "paper" 
+            ? `player: ${player} \ncomputer: ${computer}\n "Computer wins" `
+            : player === "paper" && computer === "scissors"
+            ? `player: ${player} \ncomputer: ${computer}\n "Computer wins" `
+            : player === "scissor" && computer === "rock" 
+            ? `player: ${player} \ncomputer: ${computer} \n "Computer wins" ` 
+            : `player: ${player} \ncomputer: ${computer} \n "player wins" `
+
+            alert(result);
+
+            let playAgain = confirm("Play Again?");
+            playAgain ? location.reload() : alert("ok, thanks for playing");
+        }
+        else {
+            alert("invalid move");
+        }
+    }
+    else {
+        alert("changed your mind?, may be later");
+    }
+} 
+
+else {
+    alert("may be later");
+}
