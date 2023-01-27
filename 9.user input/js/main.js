d53f63dddd008ccc8e689a526fe1feda56b36500
Moin Ahamed <ahamedjust@gmail.com>
2023-01-27 09:23:38 +0530

    user input (alert, confirm, prompt)

---
+++ 9.user input/js/main.js
@@ -0,0 +1,20 @@
+// user input
+
+
+// alert("hello world!");
+
+// let myBoolean = confirm("ok === true \n cancel === false");
+// console.log(myBoolean);
+
+let yourname = prompt("pleae enter you name.");
+if(yourname) {
+    console.log(yourname.length); 
+    console.log(yourname.trim().length);
+    console.log(yourname.trim());
+} else {
+    console.log("you didn't enter your name"); 
+}
+  
+
+
+
