94bc335d425a4c38d66f9429c38c534bf29ad75f
Moin Ahamed <ahamedjust@gmail.com>
2023-01-27 16:32:27 +0530

    provides reusable of code - functions

---
+++ 12.functions/js/main.js
@@ -0,0 +1,40 @@
+// function
+
+// function sum(num1, num2){
+//     return num1 + num2; 
+// }
+// console.log(sum(5,2));
+
+
+
+
+// function sum(num1, num2){
+//     if(num2 === undefined) {
+//         return num1 + num1;
+//     }
+//     return num1 + num2; 
+// }
+// console.log(sum(5)); 
+
+
+
+// getting user name from the email using function
+
+// function getUserName(mail) {
+//     return mail.slice(0, mail.indexOf("@")); 
+// }
+// console.log(getUserName("moinahamed@gmail.com"));
+
+
+
+// function toProperCase(name) {
+//     return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+// };
+
+// console.log(toProperCase("lasddASDLKJSADFasdlk"));
+                //OR 
+// const toProperCase = (name) => {
+//     return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+// };
+
+// console.log(toProperCase("adsAKDFadslkj"));
