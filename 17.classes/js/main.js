4c1fee4f7083737b5b548b7dbafb8a4aa89e961e
Moin Ahamed <ahamedjust@gmail.com>
2023-01-29 13:21:52 +0530

    still to go through some more examples..!

---
+++ 17.classes/js/main.js
@@ -0,0 +1,41 @@
+// classes 
+
+// 1
+// class pizza {
+//     constructor (pizzaType, pizzasize) {
+//         this.type = pizzaType;
+//         this.size = pizzasize;  
+//         this.crust = "original";
+//     }
+//     bake() {
+//         console.log(`baking a ${this.size} ${this.type} ${this.crust} crust pizza`);
+//     }
+// }
+
+// const myPizza = new pizza("pepperoni", "small");
+// myPizza.bake(); 
+
+
+// 2
+
+
+
+
+// 3
+class pizza {
+    curst = "original";
+    #sauce = "traditional";
+    constructor(pizzaSize) {
+        this.size = pizzaSize;
+    }
+
+    getCrust() {
+        return this.crust;
+    }
+    setCrust () {
+        this.crust = pizzaCrust; 
+    }
+}
+
+
+
