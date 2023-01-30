19ccfe22cb777695d60b448a681a9f86fc807e52
Moin Ahamed <ahamedjust@gmail.com>
2023-01-30 13:54:17 +0530

    handling error(skipped for now)

---
+++ 19.handling errors/js/main.js
@@ -0,0 +1,20 @@
+// javascript errors and error handling
+
+
+// variable = "moin"; // we can still log the variable without using "const"
+// console.log(variable);
+
+// "use strict";  // using this will throw an error(reference) if we don't use "const" or "let" 
+// variable1 = "moin";
+// console.log(variable1);
+
+// Object..create(); // this will throw a syntax error
+
+// const name = "dave";
+// name = "joe"; //type erroe as we cannot change the const value which is already have been assigned 
+
+
+
+
+
+
