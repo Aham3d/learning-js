bd134d1353997542e419a28fc69b0a2df2df2079
Moin Ahamed <ahamedjust@gmail.com>
2023-02-10 21:16:04 +0530

    can't figure out the bug, still getting the same error. both console log and the event listeners are working

--- 21.event listeners/12.functions/js/main.js
+++ 21.event listeners/12.functions/js/main.js
@@ -1,32 +1,54 @@
 // javascirpt event listeners
 
+// view1.style.display = "none";
+view2.style.display = "flex"; 
+// view3.style.display = "none";
+
+
+const view = document.querySelector("view2");
+const div = view.querySelector("div");
+const h2 = div.querySelector("h2");
 
 // syntax: addEventListener(event, function, useCapture)
 
-view1.style.display = "none ";
-view2.style.display = "flex"; 
+// //1
+// const doSomething = () => {
+//     alert("doing something");
+// }
 
-document.addEventListener("readystatechange", (event)=> {
+// h2.addEventListener("click", doSomething, false);  
+// h2.removeEventListener("click", doSomething, true);
+
+// h2.addEventListener("click", (event) => {
+//     console.log(event.target);
+//     EventTarget.textContent = "clicked";
+// })
+
+
+//2 
+document.addEventListener("readystatechange", (event) =>{
     if(event.target.readyState === "complete") {
-        console.log("readyState:complete");
+        console.log("readyState: complete");
         initApp();
-
     }
 });
 
 const initApp = () => {
-    const view = document.querySelector("#view2");
+    const view = document.querySelector("view2");
     const div = view.querySelector("div");
     const h2 = div.querySelector("h2");
 
     view.addEventListener("click", (event) => {
-        view.style.backgroundColor = "purple";
+        view.style.backgroundcolor = "purple";
     });
+
     div.addEventListener("click", (event) => {
-        div.style.backgroundColor = "blue";
+        view.style.backgroundcolor = "blue";
     });
+
     h2.addEventListener("click", (event) => {
         event.target.textContent = "clicked";
     });
-};
+}
+
 
