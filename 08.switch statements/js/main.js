dfcb6f855140bf176fc342e64024a40d249c67d0
Moin Ahamed <ahamedjust@gmail.com>
2023-04-03 14:24:14 +0530

    reviewed

--- 08.switch statements/js/main.js
+++ 08.switch statements/js/main.js
@@ -41,56 +41,56 @@
 
 
 // generating 5 random numbers
-switch(Math.floor(Math.random() * 5 + 1)) {
-    case 1:
-        console.log(1);
-        break; 
-    case 2:
-        console.log(2); 
-        break; 
-    case 3:
-        console.log(3);
-        break; 
+// switch(Math.floor(Math.random() * 5 + 1)) {
+//     case 1:
+//         console.log(1);
+//         break; 
+//     case 2:
+//         console.log(2); 
+//         break; 
+//     case 3:
+//         console.log(3);
+//         break; 
 
-    default:
-        console.log("no match");
+//     default:
+//         console.log("no match");
 
-}
+// }
 
 
 
 // switch statement for rock paper scissors
 
-// let player = "scissor";
-// let computer = "paper"; 
+let player = "scissor";
+let computer = "paper"; 
 
-// switch(player) {
-//     case computer:
-//         console.log("tie game");
-//         break; 
-//     case "rock":
-//         if(computer === "paper") {
-//             console.log("computer wins");
-//         }
-//         else {
-//             console.log('player wins')
-//         }
-//         break; 
-//     case "paper":
-//         if(computer === "scissor") {
-//             console.log("computer wins");
-//         } else {
-//             console.log("player wins");
-//         }
-//         break; 
-//     case "scissor":
-//         if(computer === "rock") {
-//             console.log("computer wins");
-//         } else {
-//             console.log("player wins");
-//         }
-//         break; 
-// }
+switch(player) {
+    case computer:
+        console.log("tie game");
+        break; 
+    case "rock":
+        if(computer === "paper") {
+            console.log("computer wins");
+        }
+        else {
+            console.log('player wins')
+        }
+        break; 
+    case "paper":
+        if(computer === "scissor") {
+            console.log("computer wins");
+        } else {
+            console.log("player wins");
+        }
+        break; 
+    case "scissor":
+        if(computer === "rock") {
+            console.log("computer wins");
+        } else {
+            console.log("player wins");
+        }
+        break; 
+}
 
 
 
